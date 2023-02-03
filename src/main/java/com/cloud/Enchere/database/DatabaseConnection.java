1cb36710b398a9569aa9b96f84703ac3bc54565b
Gandalf <91666711+GasyScript@users.noreply.github.com>
2023-02-03 23:53:45 +0300

    webservice commit(finally)

--- src/main/java/com/cloud/Enchere/database/DatabaseConnection.java
+++ src/main/java/com/cloud/Enchere/database/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String driver = "org.postgresql.Driver"; 
-    private static final String url = "postgresql://postgres:X78wfP2I133zTL42Juke@containers-us-west-196.railway.app:7978/railway"; 
+    private static final String url = "jdbc://containers-us-west-196.railway.app:7978/railway"; 
     private static final String username = "postgres"; 
     private static final String pwd = "X78wfP2I133zTL42Juke";
 
